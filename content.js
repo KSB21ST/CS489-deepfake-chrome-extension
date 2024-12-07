5758f5d070c9b7200d1aab698969e71e94fcb368
KSB21ST <21supersoo@kaist.ac.kr>
2024-12-07 23:43:04 +0900

    hover box position

--- content.js
+++ content.js
@@ -24,16 +24,42 @@ images.forEach(image => {
 // hover 시 표시할 정보를 처리
 function showImageInfo(image) {
   const infoBox = document.createElement("div");
-  infoBox.id = "hover-info-box";
-  infoBox.innerText = `Subin Kim Image URL: ${image.src}`;
-  infoBox.style.position = "fixed";
-  infoBox.style.top = "10px";
-  infoBox.style.right = "10px";
-  infoBox.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
-  infoBox.style.color = "white";
-  infoBox.style.padding = "10px";
-  infoBox.style.zIndex = "1000";
-  document.body.appendChild(infoBox);
+
+  // Top Right
+//   infoBox.id = "hover-info-box";
+//   infoBox.innerText = `Subin Kim Image URL: ${image.src}`;
+//   infoBox.style.position = "fixed";
+//   infoBox.style.top = "10px";
+//   infoBox.style.right = "10px";
+//   infoBox.style.backgroundColor = "rgba(255, 0, 0, 0.7)";
+//   infoBox.style.color = "white";
+//   infoBox.style.padding = "10px";
+//   infoBox.style.zIndex = "1000";
+
+// Middle
+    infoBox.id = "hover-info-box";
+    infoBox.innerText = `Image URL: ${image.src}`;
+    infoBox.style.position = "fixed";
+    infoBox.style.top = "50%";
+    infoBox.style.left = "50%";
+    infoBox.style.transform = "translate(-50%, -50%)"; // 중앙으로 이동
+    infoBox.style.backgroundColor = "rgba(255, 0, 0, 0.7)";
+    infoBox.style.color = "white";
+    infoBox.style.padding = "10px";
+    infoBox.style.zIndex = "1000";
+    document.body.appendChild(infoBox);
+
+    //beside image
+    // const rect = image.getBoundingClientRect(); // 이미지 위치 가져오기
+    // infoBox.id = "hover-info-box";
+    // infoBox.innerText = `Image URL: ${image.src}`;
+    // infoBox.style.position = "absolute";
+    // infoBox.style.top = `${rect.top + window.scrollY}px`; // 이미지의 상단 위치
+    // infoBox.style.left = `${rect.right + 10 + window.scrollX}px`; // 이미지의 오른쪽에 10px 띄워서 배치
+    // infoBox.style.backgroundColor = "rgba(255, 0, 0, 0.7)";
+    // infoBox.style.color = "white";
+    // infoBox.style.padding = "10px";
+    // infoBox.style.zIndex = "1000";
 }
 
 function hideImageInfo() {
